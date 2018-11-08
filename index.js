1f8425e082acba62b940ccccd554a8de71c29af6
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-08 16:56:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const bodyparser = require('body-parser');
+const fs = require('fs');
+
+
 const app = express()
+
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(
+    bodyparser.urlencoded({
+        extended: true
+    })
+);
+
+app.get('/', function (req, res) {
+    
+       let data   =JSON.stringify( req.body.result.parameters, null, 2);
+
+
+}
+
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
