9b891cc34d6fd82ca0b85ca911fdfa1072f84cff
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-09 14:59:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const bodyparser = require('body-parser');
 const fs = require('fs');
-
+global.service = {
+    name:null,
+    priority:null,
+    issue:null,
+    comment:null,
+};
 
 const app = express()
 
@@ -18,11 +23,35 @@ app.use(
 
 app.post('/reg', function (req, res) {
     
-       let data = JSON.stringify( req.body.result.parameters, null, 2);
+       /*let data = JSON.stringify( req.body.result.parameters, null, 2);
        
        fs.writeFile('service.json', data, (err) => {  
         if (err) throw err;
         console.log('Data written to file');
+    });*/
+
+    service.name =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.name;
+
+    service.issue = req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.issue;
+
+    service.priority = service.issue = req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.priority;
+
+    service.comment = service.issue = req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.comment;
+
+    return res.json({
+        speech: "Thank you",
+        displayText: "Done Ticket raised !!",
+        source:"dasd"
+
     });
 
 
