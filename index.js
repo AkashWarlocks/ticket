44ef703a59a5e73aff969fb0baa0baa77e22bfdd
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-12 11:58:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -83,12 +83,14 @@ app.post('/reg', function (req, res) {
 
     } else {
         return res.json({
+            "speech": "this text is spoken out loud if the platform supports voice interactions",
+            "displayText": "this text is displayed visually",
             "messages": {
                 "type": 1,
                 "title": "card title",
                 "subtitle": "card text",
                 "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-              },
+              }
             
             /*"data":{
                 
