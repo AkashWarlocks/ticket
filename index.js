58df30990ac56a8aa0ca00b7f0300697b15587f2
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-12 19:18:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const bodyparser = require('body-parser');
 const fs = require('fs');
-global.service =
-{
-    "tickets": [
+var service ={
+    
+    "tickets": [   
         {
             "name":null,
             "priority":null,
             "issue":null,
             "comment":null,
             "id":null
-        }
-
+            
+        }   
     ]
-}
     
+}
 
  ;
 
@@ -41,105 +41,107 @@ app.post('/reg', function (req, res) {
     });*/
     if(req.body.result.metadata.intentName == "Service_Ticket")
     {
-        service.name =
+        service.tickets.name.push(req.body.result &&
+            req.body.result.parameters &&
+            req.body.result.parameters.name);
+        
+    
+    service.tickets.issue.push(req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.issue);
+
+    service.tickets.priority.push( 
     req.body.result &&
     req.body.result.parameters &&
-    req.body.result.parameters.name;    
-    
-    service.issue = req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.issue;
+    req.body.result.parameters.priority);
 
-    service.priority = 
+    service.tickets.comment.push(  
     req.body.result &&
     req.body.result.parameters &&
-    req.body.result.parameters.priority;
-
-    service.comment =  
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.comment;
-
-    if (service.name == "") {
-        return res.json({
-            speech: "Can i Know Your name ??",
-            displayText: "Can i Know Your name",
-            source:"google"
+    req.body.result.parameters.comment);
+    if(req.body.result.metadata.intentName == "Service_Ticket")
+    {
+        if (service.tickets.name[service.tickets.name - 1] == "") {
+            return res.json({
+                speech: "Can i Know Your name ??",
+                displayText: "Can i Know Your name",
+                source:"google"
     
-        });
-
-    } else if ((service.issue)== "") {
-        return res.json({
-            speech: "What is the issue ?",
-            displayText: "What is the issue ?",
-            source:"google"
-    
-        });
-
-    } else if ((service.priority)== "") {
-        return res.json({
-            speech: "Can you tell me the priority of the issue ?",
-            displayText: "Can you tell me the priority of the issue ?",
-            source:"google"
-    
-        });
-
-    } else if ((service.comment)== "") {
-        return res.json({
-            speech: "Please give extra information about the incident ",
-            displayText: "Please give extra information about the incident ",
-            source:"google"
-    
-        });
-
-    } else {
-        service.id = Math.floor(Math.random() * 100) + 1;
-        return res.json({
-
-            "speech": "this text is spoken out loud if the platform supports voice interactions",
-            "displayText": "this text is displayed visually",
-
-            "data":{
-                "google": {
-                    "expectedUserResponse":true,
-                    "richResponse" : {
-                        "items" : [
-                            {
-                                "simpleResponse" : {
-                                    "textToSpeech":"Service ticket raised successfully. Here is your TICKET"
-                                }
-                            },
-                            {
-                                "basicCard": {
-            
-                                    "title": "SERVICE TICKET",
-                                    "subtitle": "ISSUE " +service.issue +" \n  ID - " +service.id,
-                                    "formattedText": "Priority "+service.priority
-                                        
-                                  },
-                            }
-                        ]
-                    }
-                   
-                }
-
-            },
-            "contextOut": [
-                {
-                  "name": "_actions_on_google",
-                  "lifespan": 99,
-                  "parameters": {
-                    "data": "{}"
-                  }
-                }
-              ]
-
-            
             });
-        }
-    
+
+        } else if (service.tickets.issue[service.tickets.issue - 1]== "") {
+            return res.json({
+                speech: "What is the issue ?",
+                displayText: "What is the issue ?",
+                source:"google"
+        
+            });
+
+        } else if (service.tickets.priority[service.tickets.priority -1]== "") {
+            return res.json({
+                speech: "Can you tell me the priority of the issue ?",
+                displayText: "Can you tell me the priority of the issue ?",
+                source:"google"
+        
+            });
+
+        } else if ( service.tickets.comment[service.tickets.comment -1]== "") {
+            return res.json({
+                speech: "Please give extra information about the incident ",
+                displayText: "Please give extra information about the incident ",
+                source:"google"
+        
+            });
+
+        } else {
+            service.id = Math.floor(Math.random() * 100) + 1;
+            return res.json({
+
+                "speech": "this text is spoken out loud if the platform supports voice interactions",
+                "displayText": "this text is displayed visually",
+
+                "data":{
+                    "google": {
+                        "expectedUserResponse":true,
+                        "richResponse" : {
+                            "items" : [
+                                {
+                                    "simpleResponse" : {
+                                        "textToSpeech":"Service ticket raised successfully. Here is your TICKET"
+                                    }
+                                },
+                                {
+                                    "basicCard": {
+                
+                                        "title": "SERVICE TICKET",
+                                        "subtitle": "ISSUE " +service.issue +" \n  ID - " +service.id,
+                                        "formattedText": "Priority "+service.priority
+                                            
+                                    },
+                                }
+                            ]
+                        }
+                    
+                    }
+
+                },
+                "contextOut": [
+                    {
+                        "name": "_actions_on_google",
+                        "lifespan": 99,
+                        "parameters": {
+                            "data": "{}"
+                        }
+                    }
+                ]
+
+                
+                });
+            }
+        
     } else {
         return res.json({
+            
             "speech": "this text is spoken out loud if the platform supports voice interactions",
             "displayText": "this text is displayed visually",
             "data":{
@@ -176,6 +178,6 @@ app.post('/reg', function (req, res) {
 
 }
 
+},
 )
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
