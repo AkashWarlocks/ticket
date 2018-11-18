6d83c95ecae7349527db8d78d67c0759b7b2b435
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-18 18:33:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ app.post('/reg', function (req, res) {
                 
         }else if (req.body.result.metadata.intentName == "View_Single_Ticket"){
           
-          var id_param = console.log("id in follow up"+req.body.inputs[0].arguments[0].textValue);
+          var id_param = console.log("id in follow up"+ JSON.stringify(req.body) );
       
       res.json({
         "speech": "Selected ticket",
