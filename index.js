d53893f72a40c6a538d43f5fd09f76256299cf9b
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-21 18:45:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -413,7 +413,30 @@ app.post('/reg', function (req, res) {
                       "data": {
                         "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
                         "carouselSelect": {
-                          "items":category_issue,
+                          "items": [
+                            {
+                              "optionInfo": {
+                                "key": "first title"
+                              },
+                              "description": "first description",
+                              "image": {
+                                "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+                                "accessibilityText": "first alt"
+                              },
+                              "title": "first title"
+                            },
+                            {
+                              "optionInfo": {
+                                "key": "second"
+                              },
+                              "description": "second description",
+                              "image": {
+                                "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+                                "accessibilityText": "second alt"
+                              },
+                              "title": "second title"
+                            }
+                          ]
                         }
                       }
                     }
