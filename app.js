a1f21e8dec8cbf2a1f1b401939b3874579abac99
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-26 14:21:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
 
 app.post('/service_ticket', function (req, res) {
 
-
+  console.log("The req body " + JSON.stringify(req.body, undefined, 2));
         
     if(req.body.result.metadata.intentName === "Service_Ticket")
     {
