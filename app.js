f899a74a6189da06064d2805c473ab9f8178ec6f
AkashWarlockz <37083016+AkashWarlockz@users.noreply.github.com>
2018-11-26 12:08:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(
 
 app.post('/service_ticket', function (req, res) {
 
-    console.log(JSON.stringify(req));
     console.log(JSON.stringify(req.body, undefined, 2));
         
     if(req.body.result.metadata.intentName === "Service_Ticket")
